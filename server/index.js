fcf1e5d52ff53b6f62ebfe46d9e6dfe8fa0ca7e6
verycosy <red_horse_@naver.com>
2019-12-21 01:00:42 +0900

    add: offer, answer, https

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 const express = require("express"),
+  https = require("https"),
   http = require("http"),
-  socketIO = require("socket.io");
-
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+  socketIO = require("socket.io"),
+  fs = require("fs");
 
 const PORT = 5020;
+const option = {
+  key: fs.readFileSync("./keys/server.key"),
+  cert: fs.readFileSync("./keys/server.crt")
+};
+
+const app = express();
+//const server = https.createServer(option, app);
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static("public"));
 app.get("/", (req, res) => {
@@ -15,8 +22,24 @@ app.get("/", (req, res) => {
 
 const ioHandler = socket => {
   console.log("Someone Connected");
-
   socket.on("disconnect", () => console.log("Someone Disconnected"));
+
+  socket.on("icecandidate", icecandidate => {
+    console.log("ICE Candidate Exchange");
+    socket.broadcast.emit("icecandidate", icecandidate);
+  });
+
+  socket.on("offer", sdpOffer => {
+    console.log("Someone Offered");
+    console.log(sdpOffer);
+    socket.broadcast.emit("offer", sdpOffer);
+  });
+
+  socket.on("answer", sdpAnswer => {
+    console.log("Someone Answered");
+    console.log(sdpAnswer);
+    socket.broadcast.emit("answer", sdpAnswer);
+  });
 };
 
 io.on("connection", ioHandler);
--- server/public/script.js
+++ server/public/script.js
@@ -1,5 +1,27 @@
 const socket = io();
 
+socket.on("offer", async sdpOffer => {
+  console.log(`✅ I got offer`);
+
+  if (!pc) createPeerConnection();
+  pc.setRemoteDescription(sdpOffer);
+  await getMedia();
+  // 순서주의 DOMException: Failed to execute 'addIceCandidate' on 'RTCPeerConnection': Error processing ICE candidate
+  // Why ??? https://stackoverflow.com/questions/13396071/errors-when-ice-candidates-are-received-before-answer-is-sent
+});
+
+socket.on("answer", sdpAnswer => {
+  console.log(`✅ I got answer`);
+  pc.setRemoteDescription(sdpAnswer);
+});
+
+socket.on("icecandidate", icecandidate => {
+  console.log(`✅ I got icecandidate`);
+  pc.addIceCandidate(icecandidate);
+});
+
+//TODO: 연결 위에 pc 객체 상태 바뀌나? / pc나 stream이 없을 경우 예외처리 / pcConfig가 없다면? 결과는?
+
 const localVideo = document.getElementById("localVideo");
 const remoteVideo = document.getElementById("remoteVideo");
 
@@ -19,9 +41,16 @@ const currentAudioStream = document.getElementById("currentAudioStream");
 let pc = null,
   localStream = null;
 
+const pcConfig = [
+  {
+    iceServers: [{ url: "stun:stun.l.google.com:19302" }]
+  }
+];
+
 const mediaStreamConstraints = {
   default: {
-    video: true
+    video: true,
+    audio: true
   },
   qvga: {
     video: { width: { exact: 320 }, height: { exact: 240 } }
@@ -89,29 +118,124 @@ function showObject(element, obj) {
   });
 }
 
-function getMedia() {
-  if (resoultionList.value === "desktop") {
-    navigator.mediaDevices.getDisplayMedia().then(stream => {
-      localVideo.srcObject = stream;
-    });
-  } else {
-    navigator.mediaDevices
-      .getUserMedia(mediaStreamConstraints[resoultionList.value])
-      .then(
-        stream => {
-          localVideo.srcObject = stream;
-          stream.getTracks().forEach(track => {
-            if (track.kind === "video") {
-              showObject(videoStreamInfo, track.getCapabilities());
-              showObject(currentVideoStream, track.getSettings());
-            } else {
-              showObject(audioStreamInfo, track.getCapabilities());
-              showObject(currentAudioStream, track.getSettings());
-            }
-          });
-        },
-        err => alert(err.name + " : " + err.message)
+async function getMedia() {
+  let stream = null;
+
+  try {
+    if (resoultionList.value === "desktop")
+      stream = await navigator.mediaDevices.getDisplayMedia();
+    else
+      stream = await navigator.mediaDevices.getUserMedia(
+        mediaStreamConstraints[resoultionList.value]
       );
+
+    localVideo.srcObject = stream;
+    localStream = stream;
+
+    console.log("✅ Add Local Stream Tracks to RTCPeerConnection");
+
+    stream.getTracks().forEach(track => {
+      console.log(pc.addTrack(track, localStream));
+
+      if (track.kind === "video") {
+        showObject(videoStreamInfo, track.getCapabilities());
+        showObject(currentVideoStream, track.getSettings());
+      } else {
+        showObject(audioStreamInfo, track.getCapabilities());
+        showObject(currentAudioStream, track.getSettings());
+      }
+    });
+  } catch (err) {
+    alert(err.name + " : " + err.message);
+  }
+}
+
+function eventHandler(callback) {
+  return evt => {
+    console.log(`✅ ${evt.type}`, evt);
+    if (typeof callback === "function") callback(evt);
+  };
+}
+
+const getStatus = () => {
+  if (pc) {
+    pc.getStats()
+      .then(res => showStatus(res))
+      .catch(err => console.log(err));
+  } else {
+    console.log("Not connected yet :(");
+  }
+};
+
+const showStatus = result => {
+  console.log("===================");
+  result.forEach(status => console.log(status));
+  console.log("===================");
+};
+
+function createPeerConnection() {
+  pc = new RTCPeerConnection();
+
+  console.log("✅ Create RTCPeerConnection");
+  console.log(pc);
+
+  pc.addEventListener(
+    "connectionstatechange",
+    eventHandler(({ currentTarget: { connectionState } }) => {
+      console.log(connectionState);
+      //if (connectionState === "connected") setInterval(getStatus, 1000);
+    })
+  );
+  pc.addEventListener("datachannel", eventHandler());
+  pc.addEventListener(
+    "icecandidate",
+    eventHandler(evt => {
+      if (evt.candidate) {
+        console.log("✅ Send ICE Candidate");
+        socket.emit("icecandidate", evt.candidate);
+      }
+    })
+  );
+  pc.addEventListener("icecandidateerror", eventHandler());
+  pc.addEventListener("iceconnectionstatechange", eventHandler());
+  pc.addEventListener("icegatheringstatechange", eventHandler());
+  pc.addEventListener("negotiationneeded", eventHandler());
+  pc.addEventListener("signalingstatechange", eventHandler());
+  pc.addEventListener("statsended", eventHandler());
+  pc.addEventListener(
+    "track",
+    eventHandler(evt => {
+      console.log(evt.streams); // addTrack에 localstream 안 붙여주면 [] 임
+      remoteVideo.srcObject = evt.streams[0];
+    })
+  );
+}
+
+async function offer() {
+  if (!pc) await createPeerConnection();
+
+  await getMedia();
+
+  try {
+    console.log("✅ Create SDP Offer");
+
+    const sdpOffer = await pc.createOffer();
+    pc.setLocalDescription(sdpOffer);
+    socket.emit("offer", sdpOffer);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function answer() {
+  try {
+    console.log("✅ Create SDP Answer");
+
+    const sdpAnswer = await pc.createAnswer();
+    pc.setLocalDescription(sdpAnswer);
+    socket.emit("answer", sdpAnswer);
+  } catch (err) {
+    console.log(err);
   }
 }
 
@@ -120,12 +244,32 @@ function init() {
     showUserPlatform();
     showEnumerateDeivces();
     showSupportedConstraints();
-    getMedia();
 
     resoultionList.addEventListener("change", getMedia);
+
+    offerButton.addEventListener("click", offer);
+    answerButton.addEventListener("click", answer);
   } else {
     alert("Not Supported Environment");
   }
 }
 
 init();
+
+/*
+Try calling localStream.getVideoTracks()[0].stop().
+
+For SCTP, reliable and ordered delivery is true by default.
+Notice the use of dataConstraint. Data channels can be configured to enable different types of data sharing — for example, prioritizing reliable delivery over performance.
+With SCTP, the protocol used by WebRTC data channels, reliable and ordered data delivery is on by default. When might RTCDataChannel need to provide reliable delivery of data, and when might performance be more important — even if that means losing some data?
+
+In order to set up and maintain a WebRTC call, WebRTC clients (peers) need to exchange metadata:
+
+Candidate (network) information.
+Offer and answer messages providing information about media, such as resolution and codecs.
+In other words, an exchange of metadata is required before peer-to-peer streaming of audio, video, or data can take place. This process is called signaling.
+
+What alternative messaging mechanisms might be possible? What problems might you encounter using 'pure' WebSocket?
+What issues might be involved with scaling this application? Can you develop a method for testing thousands or millions of simultaneous room requests?
+
+*/
